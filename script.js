aa245eff9c001156939b1efef26f2e373032727b
Sérgio Magno <57957273+magnosergio@users.noreply.github.com>
2023-11-03 20:18:06 +0000

    Método Cadastrar Nota

--- script.js
+++ script.js
@@ -166,5 +166,106 @@ function cadastrarDisciplina() {
         alert('Erro: ' + error);
     });
 }
+/* ----------------------------------------------------------------------- */
+// Variáveis para salvar a id dos dados selecionados no form de cadastro de notas
+var idAluno;
+var idDisciplina;
 
-function getInfo() {}
+// Função para preencher os dados no cadastro de notas
+function getInfo() {
+    // GET Alunos
+    fetch('http://18.117.70.4:8000/api/v1/alunos/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao obter os alunos');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const alunosList = document.getElementById('alunosList');
+            data.forEach(aluno => {
+                const option = document.createElement('option');
+                option.value = aluno.nome;
+                idAluno = aluno.id;
+                alunosList.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+
+    // GET Disciplinas
+    fetch('http://18.117.70.4:8000/api/v1/disciplinas/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao obter as disciplinas');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const disciplinasList = document.getElementById('disciplinasList');
+            data.forEach(disciplina => {
+                const option = document.createElement('option');
+                option.value = disciplina.nome;
+                idDisciplina = disciplina.id;
+                disciplinasList.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+}
+
+function cadastrarNota(){
+    // Obter o valor dos campos de entrada.
+    // Para os campos de Aluno e Disciplina, serão usado as id's salvas.
+    const aluno = idAluno;    
+    const ano = document.getElementById('anoInput').value;
+    const bimestre = document.getElementById('bimestreInput').value;
+    const disciplina = idDisciplina;
+    const nota = document.getElementById('notaInput').value;
+
+    // Dados a serem enviados
+    const data = {
+        bimestre: bimestre,
+        ano: ano,
+        nota: nota,
+        aluno: aluno,
+        disciplina: disciplina
+    };
+
+    // Token
+    const token = '8c3ddc34ec43cef32fc618ca085fedbb71c48b39';
+
+    // URL da API
+    const url = 'http://18.117.70.4:8000/api/v1/notas/';
+
+    // Enviar a solicitação POST para a API
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token ' + token
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (response.ok) {
+            // Limpar os campos para permitir novo cadastro
+            alunoSelected.value = '';
+            anoInput.value = '';
+            bimestreInput.value = '';
+            disciplinaSelected.value = '';
+            notaInput.value = '';
+
+            // Exibir mensagem de sucesso em um popup
+            alert('Nota cadastrado com sucesso!');
+        } else {
+            throw new Error('Erro ao cadastrar aluno');
+        }
+    })
+    .catch(error => {
+        // Exibir mensagem de erro em um popup
+        alert('Erro: ' + error);
+    });
+}
